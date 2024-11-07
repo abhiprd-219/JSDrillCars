4358d9bb9dca0bd5ee06a422f2972ce59585016f
Abhishek Pandey <abhiprd4685@gmail.com>
2024-11-07 16:35:18 +0530

    All problems solved

--- problem6.js
+++ problem6.js
@@ -1,5 +1,3 @@
-# JSDrillCars
-
 
 let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},
@@ -52,22 +50,25 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":49,"car_make":"Chrysler","car_model":"Sebring","car_year":1996},
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
 
-// ==== Problem #1 ====
-// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of:
-"Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
 
-// ==== Problem #2 ====
-// The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
-"Last car is a *car make goes here* *car model goes here*"
-
-// ==== Problem #3 ====
-// The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
-
-// ==== Problem #4 ====
-// The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
-
-// ==== Problem #5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
 
 // ==== Problem #6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+function audiAndBMWOnly(){
+    var filterCars = [];
+    for(let i = 0; i<inventory.length; i++){
+        if(inventory[i].car_make === 'Audi' || inventory[i].car_make === 'BMW'){
+            filterCars.push(inventory[i]);
+        }
+    }
+
+    return filterCars;
+}
+
+ var filtersCar = audiAndBMWOnly();
+ console.log(JSON.stringify(filtersCar));
+
+
+
+
+
